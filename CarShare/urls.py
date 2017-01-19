7f5a2e7c0e35b35f194da1286526c06d3e3c1360
meamuri <meammf@yandex.ru>
2017-01-19 17:28:51 +0300

    new urls

--- CarShare/urls.py
+++ CarShare/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'car_share'
+app_name = 'CarShare'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^trips/$', views.trips, name='trips'),
---
+++ README.md
@@ -0,0 +1,4 @@
+this is application for car sharing
+
+>python 3.5
+>django 1.10
